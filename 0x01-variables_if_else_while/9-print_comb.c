3cf876033d48c181c96f676a919f9fcbf386aeb4
YousefShata <yossef_shata_a@yahoo.com>
2023-03-17 14:16:33 -0700

    Bruh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * main function print alphabits
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar(48 + i);
+if (i != 9)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
