9a64131fcb34a99d1d0266157eeca9e3711102e9
YousefShata <yossef_shata_a@yahoo.com>
2023-03-17 09:36:10 -0700

    no printf needed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * main function display string using write
+ */
+int main(void)
+{
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t len = strlen(str);
+
+write(STDOUT_FILENO,str,len);
+
+return (0);
+}
