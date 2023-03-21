80e308f93467f169c2898f7a56930747d657d807
YousefShata <yossef_shata_a@yahoo.com>
2023-03-20 19:42:24 -0700

    2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * main function print alphabits
+ */
+int main(void)
+{
+int i, y;
+
+for (i = '0'; i < '9'; i++)
+{
+for (y = i + 1; y <= '9'; y++)
+{
+if (y != i)
+{
+putchar(i);
+putchar(y);
+if (y == '8' && i == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
