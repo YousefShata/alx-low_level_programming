60321b4306f4f83243516bbb68a1a3a7a4367fc0
YousefShata <yossef_shata_a@yahoo.com>
2023-03-21 16:17:51 -0700

    Putchar added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * main function display string using write
+ */
+int main(void)
+{
+char *str = "_putchar\n";
+size_t len = strlen(str);
+
+write(STDOUT_FILENO, str, len);
+
+return (1);
+}
