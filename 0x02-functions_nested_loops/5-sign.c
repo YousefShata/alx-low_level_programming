9f44702e4549d1e5794f13e7abbd3879cc66bb5a
YousefShata <yossef_shata_a@yahoo.com>
2023-03-21 18:49:46 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _print_sign - Check descpriton
+ * @n: Input char
+ *  Return: 1 if the number is positive -1 if negative or 0 of it equal 0
+ *  _print_sign check if the char c lower case or not
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
