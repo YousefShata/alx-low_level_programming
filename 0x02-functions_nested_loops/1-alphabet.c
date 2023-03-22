342d0c188689657c83485888e0fabb973b0e336b
YousefShata <yossef_shata_a@yahoo.com>
2023-03-21 17:32:09 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print alphabets
+ *
+ * Return nothing
+ *
+ *  print_alphabet print the alphabets
+ */
+void print_alphabet(void)
+{
+char C;
+
+for (C = 'a' ; C <= 'z' ; C++)
+_putchar(C);
+
+_putchar('\n');
+}
+
