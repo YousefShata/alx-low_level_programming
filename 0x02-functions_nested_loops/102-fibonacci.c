9c6c5910562697ce5e1f55fcd79dd9a8c2b89e3a
YousefShata <yossef_shata_a@yahoo.com>
2023-03-22 20:15:09 -0700

    BETTY

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * main function check the last digit in number
+ */
 int main(void)
 {
 long int x = 1, y = 2;
