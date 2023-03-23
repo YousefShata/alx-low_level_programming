e7d6103652061821225ac1abb0c847ddf461df19
YousefShata <yossef_shata_a@yahoo.com>
2023-03-22 19:08:53 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Check description
+ * @n: Starting point to print
+ * Return: nothing
+ * print_to_98 print the natural item start form n to 98
+ */
+void print_to_98(int n)
+{
+while (n != 98)
+{
+printf("%d", n);
+if (n > 98)
+{
+n--;
+printf(" ,");
+}
+else if (n < 98)
+{
+n++;
+printf(" ,");
+}
+}
+printf("%d\n", n);
+}
