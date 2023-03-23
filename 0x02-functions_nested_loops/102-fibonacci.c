61642c0cd6548b9235bac4ab9a2e90ba5d020608
YousefShata <yossef_shata_a@yahoo.com>
2023-03-22 19:54:12 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,13 +6,13 @@ long int x = 1, y = 2;
 long flag;
 int i;
 printf("%ld, %ld", x, y);
-for (i = 0; i < 50 ; i++)
+for (i = 0; i < 48 ; i++)
 {
 printf(", ");
 flag = y;
 y = y + x;
 x = flag;
-printf("%ld",y);
+printf("%ld", y);
 }
 return (0);
 }
