61cf3d42c4e4ff619250a0684dbd2944c3deb6bc
YousefShata <yossef_shata_a@yahoo.com>
2023-03-24 16:14:55 -0700

    Multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Check descpriton
+ * @a: Input int
+ * @b: Input int
+ * Return: the multiply of 2 numbers
+ * mul : multiply 2 numbers
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
