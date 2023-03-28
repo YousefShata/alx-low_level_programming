e9ee083d5567c2d9d5963dcb178c98f2e58cbcb2
YousefShata <yossef_shata_a@yahoo.com>
2023-03-27 19:32:17 -0700

    98 BETTY

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 : Change the value of pointer
+ * reset_to_98 - Change the value of pointer
  * return : nothing
- * @*n : input pointer
+ * @n : input pointer
  */
 void reset_to_98(int *n)
 {
