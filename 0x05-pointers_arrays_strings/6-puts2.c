e3cdd3b27f91e8dfab5e064b503b63be06a0e54d
YousefShata <yossef_shata_a@yahoo.com>
2023-03-28 18:23:34 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 int len = strlen(str);
 int i;
 
-for (i = 0 ; i <= len + 1; i += 2 )
+for (i = 0 ; i <= len + 1; i += 2)
 {
-if (str[i] == '\0')
+if (i == len)
 break;
 _putchar(str[i]);
 }
