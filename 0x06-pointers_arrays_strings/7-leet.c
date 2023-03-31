96661e749bed9d7d70c9a6e50bdae2549401019f
YousefShata <yossef_shata_a@yahoo.com>
2023-03-30 19:10:59 -0700

    1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - change the language to 1337
+ * @str: input string
+ * Return: string with 1337
+ */
+char *leet(char *str)
+{
+char *alp = "aAeEoOtTlL";
+char *numb = "4433007711";
+int i ;
+int j ;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; alp[j] != '\0'; j++)
+{
+if (str[i] == alp[j])
+
+str[i] = numb[j];
+}
+}
+return (str);
+}
