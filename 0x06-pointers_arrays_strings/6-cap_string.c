33749fcf1dab6e4d30569e3be6055c9f5310bf82
YousefShata <yossef_shata_a@yahoo.com>
2023-03-30 17:26:29 -0700

    CAPS

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -32,7 +32,7 @@ while (str[i])
 if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 str[i] = str[i] - 32;
 
-else if (tabulation(str[i]) && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+if (tabulation(str[i]) && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 str[i + 1] = str[i + 1] - 32;
 
 i++;
