299d1353af427d1f8ae3a50f5271a10aff959ad8
YousefShata <yossef_shata_a@yahoo.com>
2023-03-31 19:33:03 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,8 @@
 #include "string.h"
 /**
  * _strcmp - compare 2 strings
- * @str1: the first string
- * @str2: the second string
+ * @s1: the first string
+ * @s2: the second string
  * Return: 0 if both are the same , positive if bigger , negative if smaller
  */
 int _strcmp(char *s1, char *s2)
