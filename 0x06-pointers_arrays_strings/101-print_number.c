d6fbdaedb3e5d9f2da2ea948cee3b0cd1f0ffcb8
YousefShata <yossef_shata_a@yahoo.com>
2023-03-31 19:31:30 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_number - print a number
+ * @int: a number
+ * Return: a number string
+ */
+void print_number(int n)
+{
+_putchar(48 + n);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strcmp - compare 2 strings
+ * @str1: the first string
+ * @str2: the second string
+ * Return: 0 if both are the same , positive if bigger , negative if smaller
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
