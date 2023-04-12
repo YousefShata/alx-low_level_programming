0fb574aa0b1fc60cc04dd80b71a0025728890e21
YousefShata <yossef_shata_a@yahoo.com>
2023-04-12 06:11:20 -0700

    2d arr

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,9 +16,19 @@ return (NULL);
 
 p = (int **)malloc(height * sizeof(int));
 
+if (p == NULL)
+return (NULL);
+
 for (i = 0; i < height; i++)
 {
 p[i] = (int *)malloc(width * sizeof(int));
+
+if (p[i] == NULL)
+return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
 for (j = 0; j < width; j++)
 {
 p[i][j] = 0;
