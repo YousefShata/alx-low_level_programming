9c6a77c52e151d67976742397e8ff6c8a7381cc7
YousefShata <yossef_shata_a@yahoo.com>
2023-04-15 19:25:34 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name of the prgoramm
+ * @argc: arguments counter
+ * @argv: pointer to the arrguments
+ * Return: 0 to indicate sucess , 1 on failing
+ */
+int main(int args, char *argv[])
+{
+if (args != 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
