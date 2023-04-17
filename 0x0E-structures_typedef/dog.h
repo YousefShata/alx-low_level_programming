bd56b847540cf959b843abed01cf0b69b38228a2
YousefShata <yossef_shata_a@yahoo.com>
2023-04-17 11:16:09 -0700

    ogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOGS_H
 #define DOGS_H
-
+/**
+ * struct dog - dogs struct
+ * @name: name of the dog
+ * @age: float age of the dog
+ * @owner: name of the owner
+ */
 struct dog
 {
 	char *name;
