c364aaad29d3b5f4f46ece362ba18465aeaa56e8
YousefShata <yossef_shata_a@yahoo.com>
2023-04-17 14:10:56 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,11 +21,11 @@ if (d != NULL)
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
-	printf("Name: (nil)\n");
+	printf("Owner: (nil)\n");
 	}
 	else
 	{
-		printf("Name: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
 
