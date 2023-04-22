f242bcc4a5773f9f57b25027c00828f6ed9b554f
YousefShata <yossef_shata_a@yahoo.com>
2023-04-21 19:50:46 -0700

    Betty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -20,7 +20,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 /**
@@ -31,7 +31,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 /**
@@ -46,7 +46,7 @@ if (b == 0)
 {
 	printf("Error\n");
 	exit(100);
-} 
+}
 return (a / b);
 }
 
@@ -58,10 +58,10 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
-if (b ==0)
+if (b == 0)
 {
 	printf("Error\n");
 	exit(100);
 }
-        return (a % b);
+	return (a % b);
 }
