fe5db49feea21dab8672bd41f59cb9b47ab5c594
YousefShata <yossef_shata_a@yahoo.com>
2023-06-12 09:42:21 -0700

    Betty main

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - print string
+ * Return: nothing
+ */
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"");
+_puts("\t- Kevin Mitnick");
+return (0);
+}
