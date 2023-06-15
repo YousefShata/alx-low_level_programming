cc347bb87dc800181ad8d82ff3f4fa1a121e3e2d
YousefShata <yossef_shata_a@yahoo.com>
2023-06-14 19:55:34 -0700

    error handeled

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,13 +15,17 @@ int sum = 0;
 for (i = 1; i < argc; i++)
 {
 	for (j = 0; argv[i][j]; j++)
-	{
-		if (isdigit(argv[i][j] == 0))
-		{
-			puts("Error");
-			return (1);
-		}
+        {
+                if (isalpha(argv[i][j]))
+                {
+                        printf("Error\n");
+                        return (1);
+                }
 	}
+}
+
+for (i = 1; i < argc; i++)
+{
 	sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
