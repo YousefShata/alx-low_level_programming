08be6ffe357202315cdf38305a3a37eec2f7bf0c
YousefShata <yossef_shata_a@yahoo.com>
2023-06-14 18:37:14 -0700

    arguments number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print name of the programm
+ *  @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char __attribute__ ((unused))  *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
