ba76db54b50bde4a663fe0c5d45544b547b436c8
YousefShata <yossef_shata_a@yahoo.com>
2023-06-21 19:54:00 -0700

    function added to header file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,14 +9,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (size <=0)
+if (size <= 0)
 	return (-1);
 if (array == NULL || cmp == NULL)
 	return (-1);
 
 for (i = 0; i < size; i++)
 {
-	if(cmp(array[i]))
+	if (cmp(array[i]))
 		return (i);
 }
 return (-1);
