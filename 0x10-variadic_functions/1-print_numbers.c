83fceacfedda27abc0e8de7a0bf622e3e274b71c
YousefShata <yossef_shata_a@yahoo.com>
2023-06-22 18:18:57 -0700

    Prints string and numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints number followed by new line
+ * @separator: separate between each number
+ * @n: number of arguments
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list ap;
+
+if (n == 0)
+	return;
+
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+{
+	if (i == 0)
+		printf("%d", va_arg(ap, int));
+	else
+	{
+		printf("%s%d", separator, va_arg(ap, int));
+	}
+}
+putchar('\n');
+va_end(ap);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - prints string followed by new line
+ * @separator: chat between each string
+ * @n: number of argumets
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list ap;
+char *next;
+va_start(ap, n);
+
+if (n == 0)
+	return;
+
+for (i = 0; i < n; i++)
+{
+next = va_arg(ap, char*);
+	if (*next == '\0')
+		next = "nil";
+
+	if (i == 0)
+		printf("%s", next);
+	else
+	{
+		printf("%s%s", separator, next);
+	}
+}
+putchar('\n');
+va_end(ap);
+}
