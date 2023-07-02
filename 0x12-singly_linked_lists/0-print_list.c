b1f8400973ab7da6dc9019d76000034d3c6af6ba
YousefShata <yossef_shata_a@yahoo.com>
2023-07-01 19:14:38 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t num = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-	
+
 		num++;
 		h = h->next;
 	}
