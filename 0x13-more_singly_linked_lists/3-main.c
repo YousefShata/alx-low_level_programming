acdf37a46671bfb1b7a83f5f7947427392bf21d1
YousefShata <yossef_shata_a@yahoo.com>
2023-07-05 00:45:09 -0700

    handling exception

--- 0x13-more_singly_linked_lists/3-main.c
+++ 0x13-more_singly_linked_lists/3-main.c
@@ -15,12 +15,7 @@ int main(void)
     head = NULL;
     add_nodeint_end(&head, 0);
     add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
+    add_nodeint_end(&head, -2);
     print_listint(head);
     return (0);
 }
