b171c7f96a51aa08393b2cd1d7ffe8a328ec7851
YousefShata <yossef_shata_a@yahoo.com>
2023-07-04 19:44:53 -0700

    i am stuck

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,8 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *nNode = malloc(sizeof(listint_t));
-	if (nNode == NULL || *head == NULL)
+	
+	if (nNode == NULL)
 		return (NULL);
 
 	nNode->n = n;
