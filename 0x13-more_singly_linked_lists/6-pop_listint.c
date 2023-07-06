983103dbdcb51c7a33882620eb24685b6c39726f
YousefShata <yossef_shata_a@yahoo.com>
2023-07-05 19:24:23 -0700

    another error

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list,
+ * and returns the head node’s data (n)
+ * @head: double pointer to head of list;
+ * Return: value at head
+ */
