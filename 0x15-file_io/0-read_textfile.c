bd8d98768098f96b5cf9ded3ad31554f42c71287
YousefShata <yossef_shata_a@yahoo.com>
2023-07-10 19:05:31 -0700

    add some checkers

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,8 +18,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	f = fopen(filename, "r");
-
-	chars = malloc(sizeof(letters) * letters);
+	if (f == NULL)
+		return (0);
+	chars = malloc(sizeof(char) * letters);
 	if (chars == NULL)
 		return (0);
 
