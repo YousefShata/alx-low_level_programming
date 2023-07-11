c94aa68245f8a62bb2f0ca6749b3ad5fa3a2b4f7
YousefShata <yossef_shata_a@yahoo.com>
2023-07-10 18:21:02 -0700

    Read from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read a textfile and print it
+ * @filename: filename
+ * @letters: the size of the file
+ * Return: the number of letters in file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t c = 0;
+	FILE *f;
+	char *chars;
+
+	if (!filename || !letters)
+		return (0);
+
+	f = fopen(filename ,"r");
+
+	chars = malloc(sizeof(letters) * letters);
+	if (chars == NULL)
+		return (0);
+
+	c = fread(chars, sizeof(char), letters, f);
+	chars[c] = '\0';
+	fclose(f);
+	printf("%s", chars); 
+	return (c);
+}
