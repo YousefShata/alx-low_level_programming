632764e8598e5fe737e267ae958577947d0d6e35
YousefShata <yossef_shata_a@yahoo.com>
2023-08-03 08:16:15 -0700

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * binary_to_uint - swap from binary to undefined int
+ * @b: the binary number
+ * Return: Binary as int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num, j;
+	int n;
+
+	if (!b)
+		return (0);
+	n = strlen(b);
+	n--;
+
+	for (j = 1, num = 0; n >= 0; n--)
+	{
+		if (b[n] == '0')
+		{
+			j *= 2;
+			continue;
+		}
+		else if (b[n] == '1')
+		{
+			num += j;
+			j *= 2;
+			continue;
+		}
+		else
+			return (0);
+	}
+	return (num);
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+bit_manipluation once again
