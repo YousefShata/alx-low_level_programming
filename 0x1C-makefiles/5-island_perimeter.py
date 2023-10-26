be660a04a152005860b63d5d31e59acf0ac9242a
YousefShata <yossef_shata_a@yahoo.com>
2023-10-25 19:59:02 -0700

    island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+isnald_permieter
+"""
+
+
+def island_perimeter(grid):
+    """
+    return the perimeter of the island described
+    """
+    rows = len(grid)
+    col = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(col):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
