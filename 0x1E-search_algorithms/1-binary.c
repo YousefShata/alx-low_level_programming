7c1afeaace1ca3c4207e9cb121414fdbda306182
YousefShata <yossef_shata_a@yahoo.com>
2024-02-07 19:06:36 -0800

    Betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,6 @@ int binary_search(int *array, size_t size, int value)
 		mid = left + (right - left) / 2;
 
 		printf("Searching in array: ");
-
 		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
