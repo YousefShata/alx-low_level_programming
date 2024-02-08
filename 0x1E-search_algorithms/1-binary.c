5a416b31add3762f4eaefe6a9eae299e24363c6f
YousefShata <yossef_shata_a@yahoo.com>
2024-02-07 18:58:47 -0800

    add size condition

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,7 +12,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t right = size - 1;
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (left <= right)
