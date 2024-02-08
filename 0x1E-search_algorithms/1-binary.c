8a90c6bbec7b36cd49ac6f4bcc0fa04a81b113fc
YousefShata <yossef_shata_a@yahoo.com>
2024-02-07 19:05:14 -0800

    found the error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,20 +11,21 @@ int binary_search(int *array, size_t size, int value)
 	size_t left = 0;
 	size_t right = size - 1;
 	size_t i;
+	size_t mid;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	while (left <= right)
 	{
-		size_t mid = left + (right - left) / 2;
+		mid = left + (right - left) / 2;
 
 		printf("Searching in array: ");
 
 		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
-			if (i < size - 1)
+			if (i < right)
 			{
 				printf(", ");
 			}
